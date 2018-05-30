92d38c2f6798f437835502ed31626ab1e23fe602
skaveesh <skaveesh@gmail.com>
2018-05-30 14:04:14 +0530

    connection to mysql created

--- src/com/smsimulator/core/DBUtils.java
+++ src/com/smsimulator/core/DBUtils.java
@@ -1,90 +1,33 @@
 package com.smsimulator.core;
 
-import java.io.Console;
-import java.sql.*;
-import java.util.*;
-import java.util.logging.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBUtils {
     private static Connection connect = null;
-    private static final String host = "jdbc:mysql://skaveesh-rds.ck3ipm1d8ett.us-east-1.rds.amazonaws.com:3306/bank_account";
-    private static final String user = "skavessh";
-    private static final String pass = "savick1234";
-    //private static final String url = host+","+user+","+pass;
+    private static final String host = "jdbc:mysql://skaveesh-rds.ck3ipm1d8ett.us-east-1.rds.amazonaws.com:3306/";
+    private static final String database = "stock_simulator_db";
+    private static final String user = "skaveesh";
+    private static final String password = "savick1234";
+    private static boolean connectionInitiated = false;
 
-    public static Connection GetNewMySqlConnection() throws Exception {
+
+    public static boolean isConnectionInitiated() throws Exception {
+        return connectionInitiated && !getMySqlConnection().isClosed();
+    }
+
+    public static Connection getMySqlConnection() throws Exception {
         try {
             // This will load the MySQL driver, each DB has its own driver
             Class.forName("com.mysql.jdbc.Driver");
             // Setup the connection with the DB
-            connect = DriverManager.getConnection("jdbc:mysql://skaveesh-rds.ck3ipm1d8ett.us-east-1.rds.amazonaws.com:3306/stock_simulator_db", "skavessh", "savick1234");
-            //connect = DriverManager.getConnection(host+","+user+","+pass);
-            System.out.print("Connected!");
-        }catch (ClassNotFoundException ex){
-            throw  ex;
-        }catch (SQLException ex){
-            throw ex;
+            connect = DriverManager.getConnection(host + database, user, password);
+            connectionInitiated = true;
+        }catch (ClassNotFoundException | SQLException | NullPointerException ex){
+            connectionInitiated = false;
         }
-        finally {
-            connect.close();
-        }
+
         return connect;
-    }
-
-    // Method Overriding
-    public static Connection GetMySqlConnection(String url) throws Exception {
-        try {
-            // This will load the MySQL driver, each DB has its own driver
-            Class.forName("com.mysql.jdbc.Driver");
-            // Setup the connection with the DB
-            connect = DriverManager.getConnection(url);
-        }catch (ClassNotFoundException ex){
-            throw ex;
-        }catch (SQLException ex){
-            throw ex;
-        }
-        finally {
-            connect.close();
-        }
-        return connect;
-    }
-
-    public static Connection getRemoteConnection() {
-        Connection con = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            String dbName = "stock_simulator_db";
-            String userName = "skavessh";
-            String password = "savick1234";
-            String hostname = "skaveesh-rds.ck3ipm1d8ett.us-east-1.rds.amazonaws.com";
-            String port = "3306";
-            String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password + "&useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&failOverReadOnly=false&maxReconnects=10";
-            System.out.println("Getting remote connection with connection string from environment variables.");
-            con = DriverManager.getConnection(jdbcUrl);
-            System.out.println("Remote connection successful.");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return con;
-    }
-
-    public static Connection testConnection() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            String url = "jdbc:mysql://stock_simulator_db.skaveesh-rds.ck3ipm1d8ett.us-east-1.rds.amazonaws.com:3306/";
-            String userName = "skavessh";
-            String password = "savick1234";
-            String dbName = "stock_simulator_db";
-            String driver = "com.mysql.jdbc.Driver";
-            Connection connection = DriverManager.getConnection(url + dbName, userName, password);
-            System.out.println("Connected!");
-            return connection;
-        }
-        catch (ClassNotFoundException e){e.printStackTrace();}
-        catch (SQLException e){e.printStackTrace();}
-
-        return null;
     }
 }
--- src/com/smsimulator/server/root/Main.java
+++ src/com/smsimulator/server/root/Main.java
@@ -13,8 +13,6 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
         // Test below methods
-        DBUtils.getRemoteConnection();
-        DBUtils.testConnection();
         Component component = new Component();
         component.getServers().add(Protocol.HTTP, 5000);
         component.getDefaultHost().attach("", new InboundRoot());
