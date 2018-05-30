5333f3110bff1aea12d5700379add9506b82ea33
skaveesh <skaveesh@gmail.com>
2018-05-30 12:47:53 +0530

    Cross-origin resource sharing (CORS) support added

--- src/com/smsimulator/server/restlets/LoginPlayerRestlet.java
+++ src/com/smsimulator/server/restlets/LoginPlayerRestlet.java
@@ -5,6 +5,7 @@ import org.restlet.Response;
 import org.restlet.Restlet;
 import org.restlet.data.Method;
 import org.restlet.data.Status;
+
 import com.smsimulator.gsoncore.LoginPlayer;
 import com.smsimulator.server.root.InboundRoot;
 
@@ -17,9 +18,9 @@ public class LoginPlayerRestlet extends Restlet {
         if (request.getMethod().equals(Method.POST)) {
             LoginPlayer loginPlayer = InboundRoot.gson.fromJson(request.getEntityAsText(), LoginPlayer.class);
 
-            if (loginPlayer.getAuthorization().getUsername().equals("hello") && loginPlayer.getAuthorization().getPassword().equals("123456"))
+            if (loginPlayer.getAuthorization().getUsername().equals("hello") && loginPlayer.getAuthorization().getPassword().equals("123456")) {
                 response.setStatus(Status.SUCCESS_OK);
-            else
+            }else
                 response.setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
 
         } else {
--- src/com/smsimulator/server/root/Main.java
+++ src/com/smsimulator/server/root/Main.java
@@ -1,19 +1,34 @@
 package com.smsimulator.server.root;
 
-import com.smsimulator.core.CompanyShares;
 import org.restlet.Component;
 import org.restlet.data.Protocol;
+import org.restlet.service.CorsService;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * Created by skaveesh on 2018-05-21.
  */
 
-public class Main {
+public class Main extends InboundRoot {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
+
+        //Cross-origin resource sharing (CORS) support
+        CorsService corsService = new CorsService();
+        corsService.setAllowingAllRequestedHeaders(true);
+        corsService.setAllowedOrigins(new HashSet(Arrays.asList("*")));
+        corsService.setAllowedCredentials(true);
+        corsService.setSkippingResourceForCorsOptions(true);
+
+        Main inboundRoot = new Main();
+        inboundRoot.getServices().add(corsService);
+
         Component component = new Component();
+        component.getDefaultHost().attachDefault(inboundRoot);
         component.getServers().add(Protocol.HTTP, 5000);
-        component.getDefaultHost().attach("", new InboundRoot());
+        component.getDefaultHost().attach("", inboundRoot);
         component.start();
     }
 }
