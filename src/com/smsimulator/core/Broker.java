cd0c56e25071b5ec37cd02a166beb415b775fa8f
C2K <chulankotudurage@gmail.com>
2018-06-12 22:23:07 +0530

    Broker class logic added To be reviewed...

--- src/com/smsimulator/core/Broker.java
+++ src/com/smsimulator/core/Broker.java
@@ -12,13 +12,15 @@ public class Broker {
 
     private PreparedStatement preparedStatement;
     private ResultSet resultSet;
+    Bank bank = new Bank();
+    Player player = new Player();
 
     public boolean createAccount(int turn, String name) {
         int uid = new Player().getUidFromName(name);
 
         if (uid != -1) {
             try {
-                preparedStatement = DBUtils.getDatabaseConnection().prepareStatement("INSERT INTO boker_account(uid, turn, balance) VALUES(?,?,?)");
+                preparedStatement = DBUtils.getDatabaseConnection().prepareStatement("INSERT INTO broker_account(uid, turn, balance) VALUES(?,?,?)");
                 preparedStatement.setInt(1, uid);
                 preparedStatement.setInt(2, turn);
 
@@ -45,8 +47,32 @@ public class Broker {
      * @param quantity is stock quantity
      * @param price    is single stock current price
      */
-    public void buy(int turn, String name, String stock, int quantity, double price) {
+    public boolean buy(int turn, String name, String stock, int quantity, double price) {
+        int uid = player.getUidFromName(name);
+        double balance = bank.balance(name); // (Check balance and throw error if insufficient funds) This is to my knowledge handled in the stored procedure which works with the withdraw function
+        double amount = (double)quantity * price;
+        // Check if balace is more than quantity * price
+        if(balance < amount){
+            return false;
+        }
+        bank.withdraw(turn, name, stock, amount);
 
+        if(uid != -1){
+            try{
+                preparedStatement = DBUtils.getDatabaseConnection().prepareStatement("UPDATE buy_stock SET stock=?,quantity=?,price=?,turn=? WHERE uid=?");
+                preparedStatement.setString(1, stock);
+                preparedStatement.setInt(2, quantity);
+                preparedStatement.setDouble(3, price);
+                preparedStatement.setInt(4, turn);
+                preparedStatement.setInt(5, uid);
+
+            }catch (SQLException e){
+                return false;
+            }
+        } else{
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -56,8 +82,29 @@ public class Broker {
      * @param quantity is stock quantity
      * @param price    is single stock current price
      */
-    public void sell(int turn, String name, String stock, int quantity, double price) {
+    public boolean sell(int turn, String name, String stock, int quantity, double price) {
+        int uid = player.getUidFromName(name);
+        double amount = (double)quantity * price;
+        // Have to check "The transaction fails if the player does not have the specified quantity of stock"
 
+        bank.deposit(turn, name, stock, amount);
+
+        if(uid != -1){
+            try{
+                preparedStatement = DBUtils.getDatabaseConnection().prepareStatement("UPDATE sell_stock SET stock=?,quantity=?,price=?,turn=? WHERE uid=?");
+                preparedStatement.setString(1, stock);
+                preparedStatement.setInt(2, quantity);
+                preparedStatement.setDouble(3, price);
+                preparedStatement.setInt(4, turn);
+                preparedStatement.setInt(5, uid);
+
+            }catch (SQLException e){
+                return false;
+            }
+        } else{
+            return false;
+        }
+        return true;
     }
 
 }
