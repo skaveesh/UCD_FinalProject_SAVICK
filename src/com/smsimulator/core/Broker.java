c4c9b3a726c2d99c53b8ad5f6f992e969c7ef15a
C2K <chulankotudurage@gmail.com>
2018-06-16 14:26:27 +0530

    Broker sell_stock modified.

--- src/com/smsimulator/core/Broker.java
+++ src/com/smsimulator/core/Broker.java
@@ -1,8 +1,12 @@
 package com.smsimulator.core;
 
+import com.smsimulator.server.root.Main;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by skaveesh on 2018-06-11.
@@ -12,9 +16,33 @@ public class Broker {
 
     private PreparedStatement preparedStatement;
     private ResultSet resultSet;
+    private static List<Sector> sectorList;
     Bank bank = new Bank();
     Player player = new Player();
 
+    /**
+     * generating stock as static variable
+     */
+    public static void generateNewStock() {
+        sectorList = Main.initializeStocks();
+    }
+
+    /**
+     * get sectors
+     *
+     * @return get all the sectors of the market for each game. each sector includes corresponding company stocks
+     */
+    public List<Sector> getSectorList() {
+        return sectorList;
+    }
+
+    /**
+     * create account for player under broker
+     *
+     * @param turn time of the game
+     * @param name name of the portfolio
+     * @return returns true if creation successful
+     */
     public boolean createAccount(int turn, String name) {
         int uid = new Player().getUidFromName(name);
 
@@ -32,12 +60,111 @@ public class Broker {
             return false;
     }
 
-    public void portfolio(String name) {
+    /**
+     * get the information from user portfolio
+     *
+     * @param name username of the player
+     * @return portfolio of the user which includes username, stock player owns, brought stocks, sold stocks
+     */
+    public Portfolio portfolio(String name) {
+        //get all stocks owned by the user
+        List<String> ownedStockList = new ArrayList<>();
+        List<StockQuantity> ownStockList = new ArrayList<>();
+        List<Transaction> broughtStockList = new ArrayList<>();
+        List<Transaction> soldStockList = new ArrayList<>();
 
+        try {
+            //get names of all the stocks that user have dealt with
+            preparedStatement = DBUtils.getDatabaseConnection().prepareStatement("SELECT bs.stock FROM buy_stock AS bs INNER JOIN player AS p ON bs.uid=p.uid WHERE p.username=? GROUP BY bs.stock");
+            preparedStatement.setString(1, name);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                ownedStockList.add(resultSet.getString("stock"));
+            }
+
+            //call for a mysql function to get the user currently own stocks with their quantity
+            for (String stockName : ownedStockList) {
+                preparedStatement = DBUtils.getDatabaseConnection().prepareStatement("SELECT sf_getUserStockQuantity (?, ?)");
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, stockName);
+                resultSet = preparedStatement.executeQuery();
+
+                while (resultSet.next()) {
+                    if (resultSet.getInt(1) > 0)
+                        ownStockList.add(new StockQuantity(stockName, resultSet.getInt(1)));
+                }
+            }
+
+            //get user brought stocks as a list of transactions
+            preparedStatement = DBUtils.getDatabaseConnection().prepareStatement("SELECT bs.stock, bs.quantity, bs.price, bs.turn FROM buy_stock AS bs INNER JOIN player AS p ON bs.uid=p.uid WHERE p.username=?");
+            preparedStatement.setString(1, name);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                broughtStockList.add(new Transaction(resultSet.getString("stock"), resultSet.getInt("quantity"), resultSet.getDouble("price"), resultSet.getInt("turn")));
+            }
+
+            //get all sold stocks as a list of transactions
+            preparedStatement = DBUtils.getDatabaseConnection().prepareStatement("SELECT ss.stock, ss.quantity, ss.price, ss.turn FROM sell_stock AS ss INNER JOIN player AS p ON ss.uid=p.uid WHERE p.username=?");
+            preparedStatement.setString(1, name);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                soldStockList.add(new Transaction(resultSet.getString("stock"), resultSet.getInt("quantity"), resultSet.getDouble("price"), resultSet.getInt("turn")));
+            }
+
+        } catch (SQLException e) {
+            return null;
+        }
+
+        //initialize portfolio object and return it
+        return new Portfolio(name, ownStockList, broughtStockList, soldStockList);
     }
 
-    public void price(String stock) {
+    /**
+     * get the stock array of the company
+     *
+     * @param stock stock name of the company
+     * @return stock array of the company (because stock has 20 indices which displays to
+     * player, first 10 as stock history and from 11 to 20 it will displays to
+     * user as user level up in the game in each turn for the next 10 turns)
+     */
+    public double[] price(String stock) {
+        double[] companyStockPrice = new double[20];
 
+        SECTOR_LOOP:
+        for (Sector sector : sectorList) {
+            for (CompanyStock companyStock : sector.stockList) {
+                if (companyStock.getStockName().equals(stock)) {
+                    companyStockPrice = companyStock.getStockPriceArray();
+                    break SECTOR_LOOP;
+                }
+            }
+        }
+        return companyStockPrice;
+    }
+
+    /**
+     * get stock price for certain index of the stock array
+     *
+     * @param stock                  stock name of the company
+     * @param indexOfStockPriceArray stock index of the stock array
+     * @return price of the stock in the index pass through the input parameters
+     */
+    public double price(String stock, int indexOfStockPriceArray) {
+        double companyStockPrice = -1;
+
+        SECTOR_LOOP:
+        for (Sector sector : sectorList) {
+            for (CompanyStock companyStock : sector.stockList) {
+                if (companyStock.getStockName().equals(stock)) {
+                    companyStockPrice = companyStock.getStockPrice(indexOfStockPriceArray);
+                    break SECTOR_LOOP;
+                }
+            }
+        }
+        return companyStockPrice;
     }
 
     /**
@@ -53,28 +180,28 @@ public class Broker {
         double balance = bank.balance(name); // (Check balance and throw error if insufficient funds) This is to my knowledge handled in the stored procedure which works with the withdraw function
         double amount = quantity * price;
         // Check if balace is less than quantity * price
-        if(balance < amount){
+        if (balance < amount) {
             return false;
         }
         bb = bank.withdraw(turn, name, stock, amount);
-        if(bb == true){
-            if(uid != -1){
-                try{
+        if (bb == true) {
+            if (uid != -1) {
+                try {
                     preparedStatement = DBUtils.getDatabaseConnection().prepareStatement("UPDATE buy_stock SET stock=?,quantity=?,price=?,turn=? WHERE uid=?");
                     preparedStatement.setString(1, stock);
                     preparedStatement.setInt(2, quantity);
                     preparedStatement.setDouble(3, price);
                     preparedStatement.setInt(4, turn);
                     preparedStatement.setInt(5, uid);
-
-                }catch (SQLException e){
+                    return true;
+                } catch (SQLException e) {
                     return false;
                 }
-            } else{
+            } else {
                 return false;
             }
         }
-        return true;
+        return false;
     }
 
     /**
@@ -88,26 +215,42 @@ public class Broker {
         boolean bb;
         int uid = player.getUidFromName(name);
         double amount = quantity * price;
+        List<StockQuantity> ownStockList = new ArrayList<>();
         // Have to check "The transaction fails if the player does not have the specified quantity of stock"
         bb = bank.deposit(turn, name, stock, amount);
-        if(bb == true){
-            if(uid != -1){
-                try{
-                    preparedStatement = DBUtils.getDatabaseConnection().prepareStatement("UPDATE sell_stock SET stock=?,quantity=?,price=?,turn=? WHERE uid=?");
-                    preparedStatement.setString(1, stock);
-                    preparedStatement.setInt(2, quantity);
-                    preparedStatement.setDouble(3, price);
-                    preparedStatement.setInt(4, turn);
-                    preparedStatement.setInt(5, uid);
+        if (bb == true) {
+            if (uid != -1) {
+                try {
 
-                }catch (SQLException e){
+                    // MySQL function call to get current stock along with quantity
+                    preparedStatement = DBUtils.getDatabaseConnection().prepareStatement("SELECT sf_getUserStockQuantity (?, ?)");
+                    preparedStatement.setString(1, name);
+                    preparedStatement.setString(2, stock);
+                    resultSet = preparedStatement.executeQuery();
+
+                    while (resultSet.next()) {
+                        if (resultSet.getInt(1) >= amount) {
+                            return false;
+                        } else
+
+                            // How to sell?
+
+                        preparedStatement = DBUtils.getDatabaseConnection().prepareStatement("UPDATE sell_stock SET stock=?,quantity=?,price=?,turn=? WHERE uid=?");
+                        preparedStatement.setString(1, stock);
+                        preparedStatement.setInt(2, quantity);
+                        preparedStatement.setDouble(3, price);
+                        preparedStatement.setInt(4, turn);
+                        preparedStatement.setInt(5, uid);
+                        return true;
+                    }
+                } catch (SQLException e) {
                     return false;
                 }
-            } else{
+            } else {
                 return false;
             }
         }
-        return true;
+        return false;
     }
 
 }
