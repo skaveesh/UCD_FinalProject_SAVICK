bfd9c1f3f004562f9be2ab2e498bc72903e79ed1
usernsbm <ayodyanadun@gmail.com>
2018-06-20 00:35:05 +0530

    random output

--- src/com/smsimulator/core/AIPlayer.java
+++ src/com/smsimulator/core/AIPlayer.java
@@ -1,8 +1,18 @@
 package com.smsimulator.core;
 
+import java.util.List;
+
 /**
  * Created by asusgeforce on 16/06/2018.
  */
 public class AIPlayer extends AnalyserMain {
+
+    private double[] value;
+    Broker broker = new Broker();
+
+    public double[] startToBuy(){
+        value = broker.price("TAB");
+        return value;
+    }
 
 }
