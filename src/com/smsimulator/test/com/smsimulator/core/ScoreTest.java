45c19d40f30529c0b2c8a3586ccec2ea18d47cfb
C2K <chulankotudurage@gmail.com>
2018-06-23 16:35:24 +0530

    ScoreTest modified and tested completely.

--- src/com/smsimulator/test/com/smsimulator/core/ScoreTest.java
+++ src/com/smsimulator/test/com/smsimulator/core/ScoreTest.java
@@ -5,41 +5,23 @@ import static org.junit.Assert.*;
 
 public class ScoreTest {
     private Score score = new Score("",50,50,50);
-    boolean status = true;
 
     @Test
     public void getName() throws Exception {
-        if(status) {
             assertEquals("", score.getName());
-        }
     }
 
     @Test
     public void getStartBalance() throws Exception {
         double bal = 50;
-        if(status){
             assertEquals("Account balance was not correct.", bal, score.getStartBalance(), 0);
-        }
     }
 
     @Test
     public void getEndBalance() throws Exception {
         double bal = 50;
-        if(status){
             assertEquals("Account balance was not correct.", bal, score.getEndBalance(), 0);
-        }
     }
-
-/*    public boolean getProfit() throws Exception {
-        double bal = 50;
-        if(status){
-            assertEquals("Account balance was not correct.", bal, score.getProfit(), 0);
-            return true;
-        }
-        else {
-            return false;
-        }
-    }*/
 
     @Test
     public void getProfit() throws Exception {
