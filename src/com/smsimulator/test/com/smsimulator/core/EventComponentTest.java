c17305f5c74f5901389d171bbb5d2a1e6ba96489
DilharaJayaratne <dilhara.jayz@gmail.com>
2018-06-23 22:48:54 +0530

    MarketComponetsTest, EventComponentTest updated

--- src/com/smsimulator/test/com/smsimulator/core/EventComponentTest.java
+++ src/com/smsimulator/test/com/smsimulator/core/EventComponentTest.java
@@ -10,42 +10,49 @@ import static org.junit.Assert.*;
  * Created by Dilhara on 6/23/2018.
  */
 public class EventComponentTest {
+
     private EventComponent testEventComponent = new EventComponent();
-    private double expectedArray[] = new double[7];
-    private double actualArray[] = new double[7];
+    private double testArray1[] = new double[20];
+    private double testArray2[] = new double[20];
 
     @Test
     public void eventComponentGenerator() throws Exception {
-        //create a new test Market and a test sector and a test company stock
+
+        //create a new test Market and 2 test sectors and 2 test company stocks
         Market testMarket = new Market();
         List<Sector> testSectorList = testMarket.sectorList;
-        Sector financialSector = new Sector("Financial Sector");
-        CompanyStock hattonNationalBank = new CompanyStock("Hatton National Bank", "HNB");
-        testMarket.addToMarket(financialSector);
-        financialSector.addToSector(hattonNationalBank);
+        Sector sector1 = new Sector("sector1");
+        Sector sector2 = new Sector("sector2");
+        CompanyStock stock1 = new CompanyStock("name", "stock1");
+        CompanyStock stock2 = new CompanyStock("name", "stock2");
+        testMarket.addToMarket(sector1);
+        testMarket.addToMarket(sector2);
+        sector1.addToSector(stock1);
+        sector2.addToSector(stock2);
 
         for(Sector sector: testMarket.sectorList){
             for(CompanyStock companyStock:sector.stockList){
-                for (int i = 0; i <7; i++) {
-                    expectedArray[i]=companyStock.getStockPrice(i);
-                    //System.out.println(companyStock.getStockName() + " " + companyStock.getStockPrice(i));
+                for (int i = 0; i <20; i++) {
+                    testArray1[i]=companyStock.getStockPrice(i);
+                    System.out.println(companyStock.getStockName() + " " + companyStock.getStockPrice(i));
                 }
+                System.out.println("--------");
             }
         }
 
         //System.out.println("&&&&&&&&&&&&");
 
-
         for(Sector sector: testEventComponent.eventComponentGenerator(testSectorList)){
             for(CompanyStock companyStock:sector.stockList) {
-                for (int i = 0; i <7; i++) {
-                    actualArray[i] = companyStock.getStockPrice(i);
-                    // System.out.println(companyStock.getStockName() + " " + companyStock.getStockPrice(i));
+                for (int i = 0; i <20; i++) {
+                    testArray2[i] = companyStock.getStockPrice(i);
+                    System.out.println(companyStock.getStockName() + " " + companyStock.getStockPrice(i));
                 }
+                System.out.println("________");
             }
         }
 
         //assert that the eventComponentGenerator method does not return the same inputted sector list.
-        assertNotEquals(expectedArray,actualArray);
+        assertNotEquals(testArray1,testArray2);
     }
 }
